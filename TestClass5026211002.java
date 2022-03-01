287721834b35d99d68cb5dd418caab8ab4bd0bf9
ysmnynm <100695169+ysmnynm@users.noreply.github.com>
2022-03-01 20:42:22 +0700

    Add files via upload

---
+++ TestClass5026211002.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+ 
+public class TestClass5026211002 {
+    
+    
+    
+    public static void main(String[] args) {
+    ComputeMethods5026211002 input = new ComputeMethods5026211002();
+
+	double degreesC = input.fToC(100.4);
+
+	double Hypotenuse = input.hypotenuse(6, 8);
+
+	int sum = input.roll();
+
+	}
+
+}   
+ 
